141ec3a0c293821814c59fd64155f72a3a012956
Rogerio A Serapiao <rserapa@gmail.com>
2021-01-06 09:56:34 -0300

    POST /orders

--- backend/src/main/java/com/rogerio/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/rogerio/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.rogerio.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.rogerio.dsdeliver.dto.OrderDTO;
+import com.rogerio.dsdeliver.dto.ProductDTO;
 import com.rogerio.dsdeliver.entities.Order;
+import com.rogerio.dsdeliver.entities.OrderStatus;
+import com.rogerio.dsdeliver.entities.Product;
 import com.rogerio.dsdeliver.repositories.OrderRepository;
+import com.rogerio.dsdeliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -17,10 +22,26 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll(){
 		List<Order> list = repository.findOrdersWithProducts();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto){
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), 
+				dto.getLongitude(), Instant.now(),OrderStatus.PENDING );
+		// associar os produtos pela jpa
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		order = repository.save(order);		
+		return new OrderDTO(order);
 	}
 
 }
