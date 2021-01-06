47b745d744aea1dad6d905d918405f05b4d25b83
Rogerio A Serapiao <rserapa@gmail.com>
2021-01-06 00:21:54 -0300

    GET /orders

---
+++ backend/src/main/java/com/rogerio/dsdeliver/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.rogerio.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.rogerio.dsdeliver.dto.OrderDTO;
+import com.rogerio.dsdeliver.entities.Order;
+import com.rogerio.dsdeliver.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll(){
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+
+}
