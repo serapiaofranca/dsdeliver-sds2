0176f0f484b5b3c4727072ef8c03c91003fa5f08
Rogerio A Serapiao <rserapa@gmail.com>
2021-01-07 15:04:52 -0300

    Add Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,25 @@
+import './styles.css'
+import {ReactComponent as YoutubeIcon} from './youtube.svg'
+import {ReactComponent as InstagramIcon} from './instagram.svg'
+import {ReactComponent as LinkedinIcon} from './linkedin.svg'
+
+function Footer(){
+    return(
+        <footer className="main-footer">
+            App desenvolvido durante a 2a. edição da Semana DevSuperior
+            <div className="footer-icons">
+                <a href="https://www.youtube.com/c/DevSuperior" target="_new">
+                    <YoutubeIcon />
+                </a>
+                <a href="https://www.linkedin.com/scool/devsuperior/" target="new">
+                    <LinkedinIcon />
+                </a>
+                <a href="https://www.instagram.com/devsuperior.ig/" target="_new">
+                    <InstagramIcon />
+                </a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
