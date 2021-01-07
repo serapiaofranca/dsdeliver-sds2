65dfc797ac30e94e666214139dd6442fb98df78e
Rogerio A Serapiao <rserapa@gmail.com>
2021-01-07 14:22:55 -0300

    Add Home Page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Home from './Home';
 import Navbar from './Navbar/Index'
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   );
 }
