bb982fc9505882f20c92baa880005749436d6315
Rogerio A Serapiao <rserapa@gmail.com>
2021-01-08 00:25:35 -0300

    Add Orders Cards and products, integrated with back-end

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,22 @@
+import { useEffect, useState } from 'react'
+import { fetchProducts } from '../api';
+import ProductsList from './ProductsList'
 import StepsHeader from './StepsHeader'
 import './styles.css'
+import { Product } from './types';
 
 function Orders(){
+    const [products, setProducts] = useState<Product[]>([]);
+    
+    useEffect(() => {
+      fetchProducts()
+      .then(response => setProducts(response.data))
+      .catch(error => console.log(error))
+    }, [])
     return(
-        <div className="order-class-container">   
-          <StepsHeader />             
+        <div className="orders-container">   
+          <StepsHeader /> 
+          <ProductsList products={products} />            
         </div>
     )
 }
